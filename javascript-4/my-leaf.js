19f8841308f845d1c29199abf6607d41dc68558f
Igor Pavlichenko <inbox@pieware.pro>
2019-11-02 13:25:43 +1000

    Add homework javascript #4

--- javascript-4/my-leaf.js
+++ javascript-4/my-leaf.js
@@ -1,12 +1,13 @@
-import {customElement, html, LitElement, property} from "lit-element";
+import { html, LitElement } from './node_modules/lit-element/lit-element.js'
 
-@customElement('my-leaf')
 class MyLeaf extends LitElement {
 
-    @property() id;
+  static get properties () { return {leaf: Object}};
 
-    render() {
-        html`<li><span>${this.id}</span></li>`
-    }
+  render () {
+    this.leafObj = (typeof this.leaf === 'string' ? JSON.parse(this.leaf) : this.leaf)
+    return html`<li>${this.leafObj.id}<slot></slot></li>`
+  }
 }
 
+customElements.define('my-leaf', MyLeaf)
--- javascript-4/my-tree.js
+++ javascript-4/my-tree.js
@@ -1,14 +1,17 @@
-import {customElement, html, LitElement, property} from "lit-element";
+import './my-leaf.js'
+import { html, LitElement } from './node_modules/lit-element/lit-element.js'
 
-@customElement('my-tree')
 class MyTree extends LitElement {
-    @property() id;
-    @property() items;
 
-    render() {
-        html`<ul>${this.items
-            ? html`<li><span>${this.id}</span>${this.items.map(i => html`<my-tree id=${i.id} items="${i.items}"></my-tree>`)}</li>`
-            : html`<my-leaf id="${this.id}"></my-leaf>`
-        }</ul>`
-    }
+  static get properties () { return {tree: Object}};
+
+  render () {
+    this.treeObj = (typeof this.tree === 'string' ? JSON.parse(this.tree) : this.tree)
+    return html`<ul>${this.treeObj.items && this.treeObj.items.length > 0
+      ? html`<my-leaf leaf="${JSON.stringify(this.treeObj)}">${this.treeObj.items.map(i => html`<my-tree tree="${JSON.stringify(i)}"></my-tree>`)}</my-leaf>`
+      : html`<my-leaf leaf="${JSON.stringify(this.treeObj)}"></my-leaf>`
+    }</ul>`
+  }
 }
+
+customElements.define('my-tree', MyTree)
