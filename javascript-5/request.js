01a52453088419e88b4805d00dab08adf475c6a7
Igor Pavlichenko <inbox@pieware.pro>
2019-11-15 14:46:57 +1000

    Refactor homework javascript #5

--- javascript-5/request.js
+++ javascript-5/request.js
@@ -1,5 +1,9 @@
 #!env node
 
+const http = require('http')
+const {promisify} = require('util')
+const url = 'http://127.0.0.1:8080'
+
 const argv = process.argv
 if (!argv[2] || !argv[3]) {
   process.stderr.write('Usage: $ request.js [s|p] N\n')
@@ -7,11 +11,6 @@ if (!argv[2] || !argv[3]) {
   process.stderr.write('\t\tN \t count of requests\n')
   process.exit(1)
 }
-
-const http = require('http')
-const {promisify} = require('util')
-
-const url = 'http://127.0.0.1:8080'
 
 http.get[promisify.custom] = options => new Promise((resolve, reject) => {
   http.get(options, (response) => {
@@ -25,7 +24,7 @@ const promisifyGet = promisify(http.get)
 const type = argv[2]
 const count = parseInt(argv[3])
 
-if (type === 'p') // in parallel
+const parallel = () => {
   for (let i = 0; i < count; i++) {
     http.get(url, (res) => {
       let data = ''
@@ -37,18 +36,24 @@ if (type === 'p') // in parallel
       })
     })
   }
+}
+
+const sequentially = () => (async () => {
+  for (let i = 0; i < count; i++) {
+    try {
+      const response = await promisifyGet(url)
+      let data = ''
+      response.on('data', (chunk) => data += chunk)
+      await response.end
+      console.log(new Date().toUTCString(), 'Got response sequentially: ', data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+})()
+
+if (type === 'p') // in parallel
+  parallel()
 
 if (type === 's') // sequentially
-  (async () => {
-    for (let i = 0; i < count; i++) {
-      try {
-        const response = await promisifyGet(url)
-        let data = ''
-        response.on('data', (chunk) => data += chunk)
-        await response.end
-        console.log(new Date().toUTCString(), 'Got response sequentially: ', data)
-      } catch (e) {
-        console.error(e)
-      }
-    }
-  })()
+  sequentially().then(() => {console.log('Done.')})
